c35856b26da3d4e1453dd66028df9fc007485fae
binpositive <tpqls6382@gmail.com>
2025-12-20 00:03:19 +0900

    첫화면-카드 선택!

---
+++ Assets/GameSettings.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameSettings : MonoBehaviour
+{   
+    // 4가지 카드 장수 저장
+    public static int[] cardCounts = new int[4];
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
