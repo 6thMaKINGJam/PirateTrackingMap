19ba8710c8130903370f201d963f01065bf94dba
binpositive <tpqls6382@gmail.com>
2025-12-20 01:30:04 +0900

    카드 데이터 관련 함수
    GetCardCount(int cardIndex)
    GetAllCardCounts()
    GetTotalCardCount()
    UseCard(int cardIndex)
    HasAnyCard()

--- Assets/GameSettings.cs
+++ Assets/GameSettings.cs
@@ -1,22 +1,64 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class GameSettings : MonoBehaviour
-{   
-    // 4가지 카드 장수 저장
-    public static int[] cardCounts = new int[4];
+public static class GameSettings
+{
+    public const int CARD_TYPE_COUNT = 4;
+    private static int[] cardCounts = new int[CARD_TYPE_COUNT];
 
-    // Start is called before the first frame update
-    void Start()
+    public static void SetInitialCardCounts(int[] counts)
     {
-        
+        if (counts == null || counts.Length != CARD_TYPE_COUNT)
+        {
+            Debug.LogError("Invalid card count array");
+            return;
+        }
+
+        for (int i = 0; i < CARD_TYPE_COUNT; i++)
+            cardCounts[i] = counts[i];
     }
 
-    // Update is called once per frame
-    void Update()
+    public static int GetCardCount(int cardIndex)
     {
-        
+        if (cardIndex < 0 || cardIndex >= CARD_TYPE_COUNT)
+            return 0;
+
+        return cardCounts[cardIndex];
+    }
+
+    public static int[] GetAllCardCounts()
+    {
+        int[] copy = new int[CARD_TYPE_COUNT];
+        cardCounts.CopyTo(copy, 0);
+        return copy;
+    }
+    public static int GetTotalCardCount()
+    {
+        int sum = 0;
+        for (int i = 0; i < CARD_TYPE_COUNT; i++)
+            sum += cardCounts[i];
+        return sum;
+    }
+
+    public static bool UseCard(int cardIndex)
+    {
+        if (cardIndex < 0 || cardIndex >= CARD_TYPE_COUNT)
+            return false;
+
+        if (cardCounts[cardIndex] <= 0)
+            return false;
+
+        cardCounts[cardIndex]--;
+        return true;
+    }
+
+    public static bool HasAnyCard()
+    {
+        return GetTotalCardCount() > 0;
+    }
+
+    public static void Reset()
+    {
+        for (int i = 0; i < CARD_TYPE_COUNT; i++)
+            cardCounts[i] = 0;
     }
 }
-
