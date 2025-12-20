ad0a56b803d94e57cfa5307ebaaee7304a523f28
hyewon <kimh@ewha.ac.kr>
2025-12-20 14:52:35 +0900

    Feat: 테스트용 그리드 기즈모 적용

--- Assets/Scripts/Grid/GridVisualizer.cs
+++ Assets/Scripts/Grid/GridVisualizer.cs
@@ -2,20 +2,33 @@ using UnityEngine;
 
 public class GridVisualizer : MonoBehaviour
 {
+    [Header("Grid Settings")]
+    [SerializeField] private int gridWidth = 16;
+    [SerializeField] private int gridHeight = 16;
+    [SerializeField] private float cellSize = 1f;
+
+    [Header("Gizmo Settings")]
+    [SerializeField] private Color gridColor = Color.green;
+
+    // 이 조건문 제거하고 항상 그리기
     private void OnDrawGizmos()
     {
-        Gizmos.color = Color.gray;
+        Gizmos.color = gridColor;
 
-        // 세로선
-        for (int x = 0; x <= 16; x++)
+        // 세로 선 그리기
+        for (int x = 0; x <= gridWidth; x++)
         {
-            Gizmos.DrawLine(new Vector3(x, 0, 0), new Vector3(x, 16, 0));
+            Vector3 start = transform.position + new Vector3(x * cellSize, 0, 0);
+            Vector3 end = transform.position + new Vector3(x * cellSize, gridHeight * cellSize, 0);
+            Gizmos.DrawLine(start, end);
         }
 
-        // 가로선
-        for (int y = 0; y <= 16; y++)
+        // 가로 선 그리기
+        for (int y = 0; y <= gridHeight; y++)
         {
-            Gizmos.DrawLine(new Vector3(0, y, 0), new Vector3(16, y, 0));
+            Vector3 start = transform.position + new Vector3(0, y * cellSize, 0);
+            Vector3 end = transform.position + new Vector3(gridWidth * cellSize, y * cellSize, 0);
+            Gizmos.DrawLine(start, end);
         }
     }
 }
